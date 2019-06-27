52e088bad3640de100434049a22a220010724df1
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-27 10:12:44 -0600

    Add GET methods to server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const express = require('express')
 const app = express()
 const port = 3000
 
-
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-
-
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/api/v1/artists', (request, response) => {
+  database('artists').select()
+    .then((artists) => {
+      response.status(200).json(artists);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/songs', (request, response) => {
+  database('songs').select()
+    .then((songs) => {
+      response.status(200).json(songs);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+
+
+
